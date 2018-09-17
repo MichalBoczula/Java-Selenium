ee2ec2b6e2d6c574ccb9b8a63a4cb7c12f0bbd15
MichalBoczula <michal.boczula@gmail.com>
2018-09-17 20:40:24 +0200

    mod 23 updated

--- src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -2,15 +2,8 @@ package com.kodilla.testing2.google;
 
 import com.kodilla.testing2.config.WebDriverConfig;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-
-import java.util.function.Function;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class GoogleTestingApp {
     public static final String SEARCHFIELD = "lst-ib";
@@ -19,21 +12,8 @@ public class GoogleTestingApp {
         final WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("http://www.google.com");
 
-//        WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
-//        searchField.sendKeys("Kodilla");
-//        searchField.submit();
-
-        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-                .withTimeout(30, SECONDS)
-                .pollingEvery(5, SECONDS)
-                .ignoring(NoSuchElementException.class);
-
-        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
-            public WebElement apply(WebDriver driver) {
-                return driver.findElement(By.id(SEARCHFIELD));
-            }
-        });
-        foo.sendKeys("Kodilla");
-        foo.submit();
+        WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
+        searchField.sendKeys("Kodilla");
+        searchField.submit();
     }
 }
